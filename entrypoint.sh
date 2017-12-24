27803b4e6884d0c756ca6dee33bfbffb8a1ffb75
Gerardo Junior <me@gerardo-junior.com>
2017-12-24 03:44:51 -0300

    Adding exp login and gradlew clean in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+if [ -e "$(pwd)/android/gradlew" ] ; then
+    $(pwd)/android/gradlew clean -p $(pwd)/android/;
+fi
+
+if [ ! -z $EXPO_TOKEN ];then
+    /usr/bin/exp login -t $EXPO_TOKEN
+elif [[( ! -z $EXPO_USERNAME ) && ( ! -z $EXPO_PASSWORD )]];then
+    /usr/bin/exp login -u $EXPO_USERNAME -p $EXPO_PASSWORD
+fi
+
 /usr/bin/yarn --pure-lockfile
 
-$ANDROID_HOME/platform-tools/adb devices # Only to get authorization
+$ANDROID_HOME/platform-tools/adb start-server # Only to get authorization
 
-exec "$@"
+exec "$@"   
