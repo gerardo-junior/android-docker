8d00d3f30b9c15502cc13ffb557d0ab09cf51a7c
Gerardo Junior <me@gerardo-junior.com>
2017-12-27 03:09:36 -0300

    Updating node and install yarn wihout npm

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-if [ -e "$(ipwd)/android/gradlew" ] ; then
+if [ -e "$(pwd)/android/gradlew" ] ; then
     $(pwd)/android/gradlew clean -p $(pwd)/android/;
 fi
 
 if [ -n "${EXPO_TOKEN}" ] ; then
-    /usr/bin/exp login -t $EXPO_TOKEN
+    /usr/local/bin/exp login -t $EXPO_TOKEN
 elif [ -n "${EXPO_USERNAME}" ] && [ -n "${EXPO_PASSWORD}" ] ; then
-    /usr/bin/exp login -u $EXPO_USERNAME -p $EXPO_PASSWORD
+    /usr/local/bin/exp login -u $EXPO_USERNAME -p $EXPO_PASSWORD
 fi
 
 if [ -e "$(pwd)/yarn.lock" ] ; then
-    /usr/bin/yarn --pure-lockfile
-    /usr/bin/yarn cache clean --force
+    /usr/local/bin/yarn install --pure-lockfile
+    /usr/local/bin/yarn cache clean --force
 elif [ -e "$(pwd)/package.json" ] ; then
-    /usr/bin/npm install
-    /usr/bin/npm cache clean --force
+    /usr/local/bin/npm install
+    /usr/local/bin/npm cache clean --force
 fi
 
 $ANDROID_HOME/platform-tools/adb start-server # Only to get authorization
 
-exec "$@"   
+exec "$@"
